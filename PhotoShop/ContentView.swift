9f4989c79b0c8f951685fe87171f578707ff203d
“DalioKim” <ejxlabvlzhxm@naver.com>
2022-08-09 12:49:51 +0900

    Initial Commit

---
+++ PhotoShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoShop
+//
+//  Created by 김동현 on 2022/08/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoShop/PhotoShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoShopApp.swift
+//  PhotoShop
+//
+//  Created by 김동현 on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct PhotoShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
